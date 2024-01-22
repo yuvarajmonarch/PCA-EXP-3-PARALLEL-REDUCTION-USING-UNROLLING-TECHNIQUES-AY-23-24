f83bd15e48bd519b0895ebeba805fb893f2efd2f
Aswini-J <116248231+Aswini-J@users.noreply.github.com>
2024-01-22 14:41:33 +0530

    Update reduceInteger.cu

--- reduceInteger.cu
+++ reduceInteger.cu
@@ -195,18 +195,6 @@ __global__ void reduceUnrolling8 (int *g_idata, int *g_odata, unsigned int n)
     int *idata = g_idata + blockIdx.x * blockDim.x * 8;
 
     // unrolling 8
-    if (idx + 7 * blockDim.x < n)
-    {
-        int a1 = g_idata[idx];
-        int a2 = g_idata[idx + blockDim.x];
-        int a3 = g_idata[idx + 2 * blockDim.x];
-        int a4 = g_idata[idx + 3 * blockDim.x];
-        int b1 = g_idata[idx + 4 * blockDim.x];
-        int b2 = g_idata[idx + 5 * blockDim.x];
-        int b3 = g_idata[idx + 6 * blockDim.x];
-        int b4 = g_idata[idx + 7 * blockDim.x];
-        g_idata[idx] = a1 + a2 + a3 + a4 + b1 + b2 + b3 + b4;
-    }
 
     __syncthreads();
 
@@ -236,20 +224,6 @@ __global__ void reduceUnrollWarps8 (int *g_idata, int *g_odata, unsigned int n)
     int *idata = g_idata + blockIdx.x * blockDim.x * 8;
 
     // unrolling 8
-    if (idx + 7 * blockDim.x < n)
-    {
-        int a1 = g_idata[idx];
-        int a2 = g_idata[idx + blockDim.x];
-        int a3 = g_idata[idx + 2 * blockDim.x];
-        int a4 = g_idata[idx + 3 * blockDim.x];
-        int b1 = g_idata[idx + 4 * blockDim.x];
-        int b2 = g_idata[idx + 5 * blockDim.x];
-        int b3 = g_idata[idx + 6 * blockDim.x];
-        int b4 = g_idata[idx + 7 * blockDim.x];
-        g_idata[idx] = a1 + a2 + a3 + a4 + b1 + b2 + b3 + b4;
-    }
-
-    __syncthreads();
 
     // in-place reduction in global memory
     for (int stride = blockDim.x / 2; stride > 32; stride >>= 1)
@@ -290,18 +264,6 @@ __global__ void reduceCompleteUnrollWarps8 (int *g_idata, int *g_odata,
     int *idata = g_idata + blockIdx.x * blockDim.x * 8;
 
     // unrolling 8
-    if (idx + 7 * blockDim.x < n)
-    {
-        int a1 = g_idata[idx];
-        int a2 = g_idata[idx + blockDim.x];
-        int a3 = g_idata[idx + 2 * blockDim.x];
-        int a4 = g_idata[idx + 3 * blockDim.x];
-        int b1 = g_idata[idx + 4 * blockDim.x];
-        int b2 = g_idata[idx + 5 * blockDim.x];
-        int b3 = g_idata[idx + 6 * blockDim.x];
-        int b4 = g_idata[idx + 7 * blockDim.x];
-        g_idata[idx] = a1 + a2 + a3 + a4 + b1 + b2 + b3 + b4;
-    }
 
     __syncthreads();
 
@@ -350,19 +312,7 @@ __global__ void reduceCompleteUnroll(int *g_idata, int *g_odata,
     int *idata = g_idata + blockIdx.x * blockDim.x * 8;
 
     // unrolling 8
-    if (idx + 7 * blockDim.x < n)
-    {
-        int a1 = g_idata[idx];
-        int a2 = g_idata[idx + blockDim.x];
-        int a3 = g_idata[idx + 2 * blockDim.x];
-        int a4 = g_idata[idx + 3 * blockDim.x];
-        int b1 = g_idata[idx + 4 * blockDim.x];
-        int b2 = g_idata[idx + 5 * blockDim.x];
-        int b3 = g_idata[idx + 6 * blockDim.x];
-        int b4 = g_idata[idx + 7 * blockDim.x];
-        g_idata[idx] = a1 + a2 + a3 + a4 + b1 + b2 + b3 + b4;
-    }
-
+   
     __syncthreads();
 
     // in-place reduction and complete unroll
